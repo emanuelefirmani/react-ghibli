4dc7a3b7f81a3397f1b69379b57cd488f325921c
Emanuele Firmani <emanuelefirmani@kpmg.com>
2022-03-25 19:18:16 +0100

    MoviePage retrieves a hardcoded movie from Ghibli

--- src/Movies/MoviePage.tsx
+++ src/Movies/MoviePage.tsx
@@ -1,8 +1,31 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 export default function MoviePage() {
+    const [movie, setData] = useState<any>([]);
+
     let params = useParams();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("https://ghibliapi.herokuapp.com/films/578ae244-7750-4d9f-867b-f3cd3d6fecf4")
+            var movie = await response.json();
+
+            setData(movie);
+            }
+
+        console.log('1');
+        fetchData().catch(console.error);
+        console.log('2');
+    }, [])
+    console.log('3');
+
     let movieId = params.movieId;
-    return (<div>The movie '{movieId}'</div>);
+    return (
+        <div>
+            The movie '{movieId}':<br />
+            Id: {movie.id}<br />
+            Title: {movie.title}
+        </div>
+    );
 }
